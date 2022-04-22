bc952d1157e8a486266f0bddaac53dfb412d9d31
lkphuong <lekhphuong@gmail.com>
2022-04-22 10:19:37 +0700

    delete folder test

--- src/common/middlewares/confirm_url.middleware.ts
+++ src/common/middlewares/confirm_url.middleware.ts
@@ -12,7 +12,11 @@ export class ConfirmUrl implements NestMiddleware {
     console.log('Request...');
     const token = req.originalUrl.slice(20);
     jwt.verify(token, process.env.JWT_SECRET, (error, result) => {
-      if (error) throw new UnauthorizedException(1001);
+      if (error)
+        throw new UnauthorizedException(
+          1001,
+          'Cần phải đăng nhập trước khi gọi request',
+        );
       req.user = result;
       next();
     });
